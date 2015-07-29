2412fe7518a361dbaa482a53645c537d149249ee
vchuang <vchuang@digitalriver.com>
2015-07-29 10:23:01 +0800

    fade in / out

--- js/main.js
+++ js/main.js
@@ -1,58 +1,45 @@
 $(function() {
-	$('.kv_content').css('visibility', 'hidden');
-	var isInit = true;
+  $('.kv_content img').fadeIn('slow');
 
-  $('.kv_content').carouFredSel({
-    circular: true,
-    infinite: false,
-    auto: false,
-    responsive: true,
-    prev: {
-      button: ".btn_prev",
-      key: "left"
-    },
-    next: {
-      button: ".btn_next",
-      key: "right"
-    },
-    scroll: {
-      fx: "crossfade",
-      onBefore : function(data) {
-      	$(this).trigger("currentPosition", function(pos) {
-      		$('.nav_main').eq(pos).trigger('click');
-      	});
-      },
-      onAfter : function(data) {
-      	if ( isInit ) {
-      		$('.kv_content').css('visibility', 'visible');
-      		isInit = false;
-      	} else {
-      		window.location.href = $('.menu .nav_main.select').attr('href');
-      	}	
-      }
-    },
-    onCreate: function() {
-    	var $selectedItem = $('.menu .nav_main.select');
-    	var index = $('.menu .nav_main').index($selectedItem);
-    	if ( index > 0 ) {
-	    	$('.kv_content').trigger("slideTo", index);
-	    }	else {
-	    	$('.kv_content').css('visibility', 'visible');
-      	isInit = false;
-	    }
+  var menu_count = $('.nav_main').length;
+
+  $(document).on('click', '.btn_prev, .btn_next', function() {
+    var $selectedItem = $('.menu .nav_main.select');
+    var index = $('.menu .nav_main').index($selectedItem);
+    var next = (index < menu_count - 1) ? index + 1 : 0;
+    var prev = (index > 0) ? index - 1 : menu_count - 1;
+
+    if ($(this).hasClass('btn_prev')) {
+      $('.nav_main').eq(prev).trigger('click');
+    } else if ($(this).hasClass('btn_next')) {
+      $('.nav_main').eq(next).trigger('click');
     }
+
   });
 
   $(document).on('click', '.nav_main', function(event) {
-  	event.preventDefault();
-  	var url_link = $(this).attr('href');
-  	var index = $(this).data('index');
+    event.preventDefault();
+    var url_link = $(this).attr('href');
+    var index = $(this).data('index');
 
-  	$('.nav_main').removeClass('select');
+    $('.nav_main').removeClass('select');
 
-  	$(this).addClass('select');
-  	window.location.href = url_link;
-  	// $('.kv_content').trigger("slideTo", index-1);
+    $(this).addClass('select');
+
+    $('.kv_content img').fadeOut('slow', function() {
+      window.location.href = url_link;
+    });
+
+  });
+
+  $(document).on('keydown', function(event) {
+    var keyCode = event.keyCode;
+
+    if (keyCode == 39) {
+      $('.btn_next').trigger('click');
+    } else if (keyCode == 37) {
+      $('.btn_prev').trigger('click');
+    }
   });
 
 });
